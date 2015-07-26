adf71c2a769674a10bc4e5c7fd903d4397e224d1
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-26 21:48:40 +0200

    Logging & Clean-up

--- ScpControl/UsbHub.cs
+++ ScpControl/UsbHub.cs
@@ -1,45 +1,43 @@
 ﻿using System;
 using System.ComponentModel;
 
-namespace ScpControl 
+namespace ScpControl
 {
-    public partial class UsbHub : ScpHub 
+    public partial class UsbHub : ScpHub
     {
-        protected UsbDevice[] Device = new UsbDevice[4];
-
-
-        public UsbHub() 
+        private readonly UsbDevice[] _device = new UsbDevice[4];
+        
+        public UsbHub()
         {
             InitializeComponent();
         }
 
-        public UsbHub(IContainer container) 
+        public UsbHub(IContainer container)
         {
             container.Add(this);
 
             InitializeComponent();
         }
 
-
-        public override Boolean Open()  
+        public override Boolean Open()
         {
-            for (Byte Pad = 0; Pad < Device.Length; Pad++)
+            for (Byte Pad = 0; Pad < _device.Length; Pad++)
             {
-                Device[Pad] = new UsbDevice();
+                _device[Pad] = new UsbDevice();
 
-                Device[Pad].PadId = (DsPadId) Pad;
+                _device[Pad].PadId = (DsPadId)Pad;
             }
 
             return base.Open();
         }
 
-        public override Boolean Start() 
+        public override Boolean Start()
         {
             m_Started = true;
 
             Byte Index = 0;
 
-            for (Byte Instance = 0; Instance < Device.Length && Index < Device.Length; Instance++)
+            for (Byte Instance = 0; Instance < _device.Length && Index < _device.Length; Instance++)
             {
                 try
                 {
@@ -52,21 +50,25 @@ namespace ScpControl
                         {
                             Current.Report += new EventHandler<ReportEventArgs>(On_Report);
 
-                            Device[Index++] = Current;
+                            _device[Index++] = Current;
                         }
                         else Current.Close();
                     }
                     else Current.Close();
                 }
-                catch { break; }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("Unexpected error: {0}", ex);
+                    break;
+                }
             }
 
-            for (Byte Instance = 0; Instance < Device.Length && Index < Device.Length; Instance++)
+            for (Byte Instance = 0; Instance < _device.Length && Index < _device.Length; Instance++)
             {
                 try
                 {
                     UsbDevice Current = new UsbDs3();
-                    Current.PadId = (DsPadId)Index;
+                    Current.PadId = (DsPadId) Index;
 
                     if (Current.Open(Instance))
                     {
@@ -74,70 +76,82 @@ namespace ScpControl
                         {
                             Current.Report += new EventHandler<ReportEventArgs>(On_Report);
 
-                            Device[Index++] = Current;
+                            _device[Index++] = Current;
                         }
                         else Current.Close();
                     }
                     else Current.Close();
                 }
-                catch { break; }
+                catch (Exception ex)
+                {
+                    Log.ErrorFormat("Unexpected error: {0}", ex);
+                    break;
+                }
             }
 
             try
             {
-                for (Index = 0; Index < Device.Length; Index++)
+                for (Index = 0; Index < _device.Length; Index++)
                 {
-                    if (Device[Index].State == DsState.Reserved)
+                    if (_device[Index].State == DsState.Reserved)
                     {
-                        Device[Index].Start();
+                        _device[Index].Start();
                     }
                 }
             }
-            catch { }
+            catch (Exception ex)
+            {
+                Log.ErrorFormat("Unexpected error: {0}", ex);
+            }
 
             return base.Start();
         }
 
-        public override Boolean Stop()  
+        public override Boolean Stop()
         {
             m_Started = false;
 
             try
             {
-                for (Int32 Index = 0; Index < Device.Length; Index++)
+                for (Int32 Index = 0; Index < _device.Length; Index++)
                 {
-                    if (Device[Index].State == DsState.Connected)
+                    if (_device[Index].State == DsState.Connected)
                     {
-                        Device[Index].Stop();
+                        _device[Index].Stop();
                     }
                 }
             }
-            catch { }
+            catch (Exception ex)
+            {
+                Log.ErrorFormat("Unexpected error: {0}", ex);
+            }
 
             return base.Stop();
         }
 
-        public override Boolean Close() 
+        public override Boolean Close()
         {
             m_Started = false;
 
             try
             {
-                for (Int32 Index = 0; Index < Device.Length; Index++)
+                for (Int32 Index = 0; Index < _device.Length; Index++)
                 {
-                    if (Device[Index].State == DsState.Connected)
+                    if (_device[Index].State == DsState.Connected)
                     {
-                        Device[Index].Close();
+                        _device[Index].Close();
                     }
                 }
             }
-            catch { }
+            catch (Exception ex)
+            {
+                Log.ErrorFormat("Unexpected error: {0}", ex);
+            }
 
             return base.Close();
         }
-
-
-        public override Boolean Suspend() 
+        
+        public override Boolean Suspend()
         {
             Stop();
             Close();
@@ -145,16 +159,15 @@ namespace ScpControl
             return base.Suspend();
         }
 
-        public override Boolean Resume()  
+        public override Boolean Resume()
         {
             Open();
             Start();
 
             return base.Resume();
         }
-
-
-        public override DsPadId Notify(ScpDevice.Notified Notification, String Class, String Path) 
+        
+        public override DsPadId Notify(ScpDevice.Notified Notification, String Class, String Path)
         {
             Log.DebugFormat("++ Notify [{0}] [{1}] [{2}]", Notification, Class, Path);
 
@@ -173,12 +186,12 @@ namespace ScpControl
 
                             if (LogArrival(Arrived))
                             {
-                                if (Device[(Byte) Arrived.PadId].IsShutdown)
+                                if (_device[(Byte)Arrived.PadId].IsShutdown)
                                 {
-                                    Device[(Byte) Arrived.PadId].IsShutdown = false;
+                                    _device[(Byte)Arrived.PadId].IsShutdown = false;
 
-                                    Device[(Byte) Arrived.PadId].Close();
-                                    Device[(Byte) Arrived.PadId] = Arrived;
+                                    _device[(Byte)Arrived.PadId].Close();
+                                    _device[(Byte)Arrived.PadId] = Arrived;
 
                                     return Arrived.PadId;
                                 }
@@ -186,8 +199,8 @@ namespace ScpControl
                                 {
                                     Arrived.Report += new EventHandler<ReportEventArgs>(On_Report);
 
-                                    Device[(Byte) Arrived.PadId].Close();
-                                    Device[(Byte) Arrived.PadId] = Arrived;
+                                    _device[(Byte)Arrived.PadId].Close();
+                                    _device[(Byte)Arrived.PadId] = Arrived;
 
                                     if (m_Started) Arrived.Start();
                                     return Arrived.PadId;
@@ -201,13 +214,13 @@ namespace ScpControl
 
                 case ScpDevice.Notified.Removal:
                     {
-                        for (Int32 Index = 0; Index < Device.Length; Index++)
+                        for (Int32 Index = 0; Index < _device.Length; Index++)
                         {
-                            if (Device[Index].State == DsState.Connected && Path == Device[Index].Path)
+                            if (_device[Index].State == DsState.Connected && Path == _device[Index].Path)
                             {
-                                Log.DebugFormat("-- Device Removal [{0}]", Device[Index].Local);
+                                Log.DebugFormat("-- Device Removal [{0}]", _device[Index].Local);
 
-                                Device[Index].Stop();
+                                _device[Index].Stop();
                             }
                         }
                     }
