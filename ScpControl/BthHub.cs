b2b8e93b5acea4152f3392e87fc98ef74c9216c6
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-27 00:30:35 +0200

    Major refactoring

--- ScpControl/BthHub.cs
+++ ScpControl/BthHub.cs
@@ -1,84 +1,82 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
-namespace ScpControl 
+namespace ScpControl
 {
-    public partial class BthHub : ScpHub 
+    public partial class BthHub : ScpHub
     {
-        protected BthDongle Device;
+        private BthDongle _device;
 
-        public String  Dongle   
-        {
-            get { return Device.ToString(); }
-        }
-        public String  Master   
-        {
-            get { return Device.Local; }
-        }
-        public Boolean Pairable 
-        {
-            get { return m_Started && Device.State == DsState.Connected && Device.Initialised; }
-        }
-
-
-        public BthHub() 
+        public BthHub()
         {
             InitializeComponent();
         }
 
-        public BthHub(IContainer container) 
+        public BthHub(IContainer container)
         {
             container.Add(this);
 
             InitializeComponent();
         }
 
-
-        public override Boolean Open()  
+        public string Dongle
         {
-            Device = new BthDongle();
+            get { return _device.ToString(); }
+        }
 
-            Device.Arrival += new EventHandler<ArrivalEventArgs>(On_Arrival);
-            Device.Report  += new EventHandler<ReportEventArgs> (On_Report);
-            
-            if (!Device.Open()) Device.Close();
+        public string Master
+        {
+            get { return _device.Local; }
+        }
+
+        public bool Pairable
+        {
+            get { return m_Started && _device.State == DsState.Connected && _device.Initialised; }
+        }
+
+        public override bool Open()
+        {
+            _device = new BthDongle();
+
+            _device.Arrival += On_Arrival;
+            _device.Report += On_Report;
+
+            if (!_device.Open()) _device.Close();
 
             return true;
         }
 
-        public override Boolean Start() 
+        public override bool Start()
         {
             m_Started = true;
 
-            if (Device.State == DsState.Reserved)
+            if (_device.State == DsState.Reserved)
             {
-                Device.Start();
+                _device.Start();
             }
 
             return m_Started;
         }
 
-        public override Boolean Stop()  
+        public override bool Stop()
         {
             m_Started = false;
 
-            if (Device.State == DsState.Connected)
+            if (_device.State == DsState.Connected)
             {
-                Device.Stop();
+                _device.Stop();
             }
 
             return !m_Started;
         }
 
-        public override Boolean Close() 
+        public override bool Close()
         {
             m_Started = false;
 
-            return Device.Close();
+            return _device.Close();
         }
 
-
-        public override Boolean Suspend() 
+        public override bool Suspend()
         {
             Stop();
             Close();
@@ -86,7 +84,7 @@ namespace ScpControl
             return base.Suspend();
         }
 
-        public override Boolean Resume()  
+        public override bool Resume()
         {
             Open();
             Start();
@@ -94,46 +92,45 @@ namespace ScpControl
             return base.Resume();
         }
 
-
-        public override DsPadId Notify(ScpDevice.Notified Notification, String Class, String Path) 
+        public override DsPadId Notify(ScpDevice.Notified Notification, string Class, string Path)
         {
             Log.DebugFormat("++ Notify [{0}] [{1}] [{2}]", Notification, Class, Path);
 
             switch (Notification)
             {
                 case ScpDevice.Notified.Arrival:
+                {
+                    if (_device.State != DsState.Connected)
                     {
-                        if (Device.State != DsState.Connected)
+                        var arrived = new BthDongle();
+
+                        if (arrived.Open(Path))
                         {
-                            BthDongle Arrived = new BthDongle();
+                            Log.DebugFormat("-- Device Arrival [{0}]", arrived.Local);
 
-                            if (Arrived.Open(Path))
-                            {
-                                Log.DebugFormat("-- Device Arrival [{0}]", Arrived.Local);
+                            _device.Close();
+                            _device = arrived;
 
-                                Device.Close();
-                                Device = Arrived;
+                            _device.Arrival += On_Arrival;
+                            _device.Report += On_Report;
 
-                                Device.Arrival += new EventHandler<ArrivalEventArgs>(On_Arrival);
-                                Device.Report  += new EventHandler<ReportEventArgs> (On_Report );
-
-                                if (m_Started) Device.Start();
-                                break;
-                            }
-
-                            Arrived.Close();
-                            Arrived.Dispose();
+                            if (m_Started) _device.Start();
+                            break;
                         }
+
+                        arrived.Close();
+                        arrived.Dispose();
                     }
+                }
                     break;
 
                 case ScpDevice.Notified.Removal:
 
-                    if (Device.Path == Path)
+                    if (_device.Path == Path)
                     {
-                        Log.DebugFormat("-- Device Removal [{0}]", Device.Local);
+                        Log.DebugFormat("-- Device Removal [{0}]", _device.Local);
 
-                        Device.Stop();
+                        _device.Stop();
                     }
                     break;
             }
