38bc89f2a39d0f8f7ab12aba26764f589f870e38
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-26 23:49:47 +0200

    More clean-up

--- ScpServer/ScpForm.cs
+++ ScpServer/ScpForm.cs
@@ -7,19 +7,17 @@ using log4net;
 using ScpControl;
 using ScpServer.Properties;
 
-namespace ScpServer 
+namespace ScpServer
 {
-    public partial class ScpForm : Form 
+    public partial class ScpForm : Form
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-
+        private readonly RadioButton[] Pad = new RadioButton[4];
+        private IntPtr m_BthNotify = IntPtr.Zero;
         private IntPtr m_Ds3Notify = IntPtr.Zero;
         private IntPtr m_Ds4Notify = IntPtr.Zero;
-        private IntPtr m_BthNotify = IntPtr.Zero;
 
-        private RadioButton[] Pad = new RadioButton[4];
-
-        public ScpForm() 
+        public ScpForm()
         {
             InitializeComponent();
             ThemeUtil.SetTheme(lvDebug);
@@ -30,21 +28,22 @@ namespace ScpServer
             Pad[3] = rbPad_4;
         }
 
-        private void Form_Load(object sender, EventArgs e) 
+        private void Form_Load(object sender, EventArgs e)
         {
             Icon = Resources.Scp_All;
 
-            ScpDevice.RegisterNotify(Handle, new Guid(UsbDs3.USB_CLASS_GUID   ), ref m_Ds3Notify);
-            ScpDevice.RegisterNotify(Handle, new Guid(UsbDs4.USB_CLASS_GUID   ), ref m_Ds4Notify);
+            ScpDevice.RegisterNotify(Handle, new Guid(UsbDs3.USB_CLASS_GUID), ref m_Ds3Notify);
+            ScpDevice.RegisterNotify(Handle, new Guid(UsbDs4.USB_CLASS_GUID), ref m_Ds4Notify);
             ScpDevice.RegisterNotify(Handle, new Guid(BthDongle.BTH_CLASS_GUID), ref m_BthNotify);
 
-            Log.DebugFormat("++ {0} [{1}]", Assembly.GetExecutingAssembly().Location, Assembly.GetExecutingAssembly().GetName().Version);
+            Log.DebugFormat("++ {0} [{1}]", Assembly.GetExecutingAssembly().Location,
+                Assembly.GetExecutingAssembly().GetName().Version);
 
             tmrUpdate.Enabled = true;
             btnStart_Click(sender, e);
         }
 
-        private void Form_Close(object sender, FormClosingEventArgs e) 
+        private void Form_Close(object sender, FormClosingEventArgs e)
         {
             rootHub.Stop();
             rootHub.Close();
@@ -54,42 +53,43 @@ namespace ScpServer
             if (m_BthNotify != IntPtr.Zero) ScpDevice.UnregisterNotify(m_BthNotify);
         }
 
-        private void btnStart_Click(object sender, EventArgs e) 
+        private void btnStart_Click(object sender, EventArgs e)
         {
             if (rootHub.Open() && rootHub.Start())
             {
                 btnStart.Enabled = false;
-                btnStop.Enabled  = true;
+                btnStop.Enabled = true;
             }
         }
 
-        private void btnStop_Click(object sender, EventArgs e) 
+        private void btnStop_Click(object sender, EventArgs e)
         {
             if (rootHub.Stop())
             {
                 btnStart.Enabled = true;
-                btnStop.Enabled  = false;
+                btnStop.Enabled = false;
             }
         }
 
-        private void btnClear_Click(object sender, EventArgs e) 
+        private void btnClear_Click(object sender, EventArgs e)
         {
             lvDebug.Items.Clear();
         }
 
-        private void btnMotor_Click(object sender, EventArgs e) 
+        private void btnMotor_Click(object sender, EventArgs e)
         {
-            Button Target = (Button) sender;
-            Byte Left = 0x00, Right = 0x00;
+            var Target = (Button) sender;
+            byte Left = 0x00, Right = 0x00;
 
             if (Target == btnBoth)
             {
-                Left = 0xFF; Right = 0xFF;
+                Left = 0xFF;
+                Right = 0xFF;
             }
-            else if (Target == btnLeft ) Left  = 0xFF;
+            else if (Target == btnLeft) Left = 0xFF;
             else if (Target == btnRight) Right = 0xFF;
 
-            for (int Index = 0; Index < 4; Index++)
+            for (var Index = 0; Index < 4; Index++)
             {
                 if (Pad[Index].Enabled && Pad[Index].Checked)
                 {
@@ -98,18 +98,18 @@ namespace ScpServer
             }
         }
 
-        private void btnPair_Click(object sender, EventArgs e) 
+        private void btnPair_Click(object sender, EventArgs e)
         {
-            for (Int32 Index = 0; Index < Pad.Length; Index++)
+            for (var Index = 0; Index < Pad.Length; Index++)
             {
                 if (Pad[Index].Checked)
                 {
-                    Byte[]   Master = new Byte[6];
-                    String[] Parts  = rootHub.Master.Split(new String[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+                    var Master = new byte[6];
+                    var Parts = rootHub.Master.Split(new[] {":"}, StringSplitOptions.RemoveEmptyEntries);
 
-                    for (Int32 Part = 0; Part < Master.Length; Part++)
+                    for (var Part = 0; Part < Master.Length; Part++)
                     {
-                        Master[Part] = Byte.Parse(Parts[Part], NumberStyles.HexNumber);
+                        Master[Part] = byte.Parse(Parts[Part], NumberStyles.HexNumber);
                     }
 
                     rootHub.Pad[Index].Pair(Master);
@@ -118,9 +118,9 @@ namespace ScpServer
             }
         }
 
-        protected void btnDisconnect_Click(object sender, EventArgs e) 
+        protected void btnDisconnect_Click(object sender, EventArgs e)
         {
-            for (Int32 Index = 0; Index < Pad.Length; Index++)
+            for (var Index = 0; Index < Pad.Length; Index++)
             {
                 if (Pad[Index].Checked)
                 {
@@ -130,23 +130,23 @@ namespace ScpServer
             }
         }
 
-        protected void btnSuspend_Click(object sender, EventArgs e) 
+        protected void btnSuspend_Click(object sender, EventArgs e)
         {
             rootHub.Suspend();
         }
 
-        protected void btnResume_Click(object sender, EventArgs e) 
+        protected void btnResume_Click(object sender, EventArgs e)
         {
             rootHub.Resume();
         }
 
-        protected override void WndProc(ref Message m) 
+        protected override void WndProc(ref Message m)
         {
             try
             {
                 if (m.Msg == ScpDevice.WM_DEVICECHANGE)
                 {
-                    Int32 Type = m.WParam.ToInt32();
+                    var Type = m.WParam.ToInt32();
 
                     switch (Type)
                     {
@@ -169,9 +169,9 @@ namespace ScpServer
                                         Marshal.PtrToStructure(m.LParam,
                                             typeof (ScpDevice.DEV_BROADCAST_DEVICEINTERFACE_M));
 
-                                String Class = "{" + new Guid(deviceInterface.dbcc_classguid).ToString().ToUpper() + "}";
+                                var Class = "{" + new Guid(deviceInterface.dbcc_classguid).ToString().ToUpper() + "}";
 
-                                String Path = new String(deviceInterface.dbcc_name);
+                                var Path = new string(deviceInterface.dbcc_name);
                                 Path = Path.Substring(0, Path.IndexOf('\0')).ToUpper();
 
                                 rootHub.Notify((ScpDevice.Notified) Type, Class, Path);
@@ -188,22 +188,25 @@ namespace ScpServer
             base.WndProc(ref m);
         }
 
-        private void tmrUpdate_Tick(object sender, EventArgs e) 
+        private void tmrUpdate_Tick(object sender, EventArgs e)
         {
-            Boolean bSelected = false, bDisconnect = false, bPair = false;
+            bool bSelected = false, bDisconnect = false, bPair = false;
 
-            lblHost.Text = rootHub.Dongle; lblHost.Enabled = btnStop.Enabled;
+            lblHost.Text = rootHub.Dongle;
+            lblHost.Enabled = btnStop.Enabled;
 
-            for (Int32 Index = 0; Index < Pad.Length; Index++)
+            for (var Index = 0; Index < Pad.Length; Index++)
             {
-                Pad[Index].Text    = rootHub.Pad[Index].ToString();
+                Pad[Index].Text = rootHub.Pad[Index].ToString();
                 Pad[Index].Enabled = rootHub.Pad[Index].State == DsState.Connected;
                 Pad[Index].Checked = Pad[Index].Enabled && Pad[Index].Checked;
 
-                bSelected   = bSelected   || Pad[Index].Checked;
+                bSelected = bSelected || Pad[Index].Checked;
                 bDisconnect = bDisconnect || rootHub.Pad[Index].Connection == DsConnection.BTH;
 
-                bPair = bPair || (Pad[Index].Checked && rootHub.Pad[Index].Connection == DsConnection.USB && rootHub.Master != rootHub.Pad[Index].Remote);
+                bPair = bPair ||
+                        (Pad[Index].Checked && rootHub.Pad[Index].Connection == DsConnection.USB &&
+                         rootHub.Master != rootHub.Pad[Index].Remote);
             }
 
             btnBoth.Enabled = btnLeft.Enabled = btnRight.Enabled = btnOff.Enabled = bSelected && btnStop.Enabled;
@@ -212,13 +215,13 @@ namespace ScpServer
 
             btnClear.Enabled = lvDebug.Items.Count > 0;
         }
-        
-        private void lvDebug_Enter(object sender, EventArgs e) 
+
+        private void lvDebug_Enter(object sender, EventArgs e)
         {
             ThemeUtil.UpdateFocus(lvDebug.Handle);
         }
 
-        private void Button_Enter(object sender, EventArgs e) 
+        private void Button_Enter(object sender, EventArgs e)
         {
             ThemeUtil.UpdateFocus(((Button) sender).Handle);
         }
