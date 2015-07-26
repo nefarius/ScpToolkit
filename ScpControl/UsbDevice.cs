840fcb0af25dc21b51491c17dab1720ae21c6d28
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-27 00:22:30 +0200

    Major refactoring

--- ScpControl/UsbDevice.cs
+++ ScpControl/UsbDevice.cs
@@ -1,136 +1,109 @@
 ﻿using System;
 using System.ComponentModel;
 
-namespace ScpControl 
+namespace ScpControl
 {
-    public partial class UsbDevice : ScpDevice, IDsDevice 
+    public partial class UsbDevice : ScpDevice, IDsDevice
     {
-        protected ReportEventArgs m_ReportArgs = new ReportEventArgs();
-
-        protected String   m_Instance = String.Empty, m_Mac = String.Empty;
-        protected Boolean  m_Publish = false;
-        protected Boolean  m_IsDisconnect = false;
+        protected byte m_BatteryStatus = 0;
+        protected byte[] m_Buffer = new byte[64];
+        protected byte m_CableStatus = 0;
+        protected byte m_ControllerId;
+        protected string m_Instance = string.Empty, m_Mac = string.Empty;
+        protected bool m_IsDisconnect;
         protected DateTime m_Last = DateTime.Now, m_Tick = DateTime.Now, m_Disconnect = DateTime.Now;
-
-        public event EventHandler<ReportEventArgs> Report = null;
-
-        protected Byte[] m_Buffer = new Byte[64];
-        protected Byte[] m_Master = new Byte[6];
-        protected Byte[] m_Local  = new Byte[6];
-
-        protected Byte m_ControllerId  = 0;
-        protected Byte m_BatteryStatus = 0;
-        protected Byte m_CableStatus   = 0;
-        protected Byte m_PlugStatus    = 0;
-        protected Byte m_Model         = 0;
-
+        protected byte[] m_Local = new byte[6];
+        protected byte[] m_Master = new byte[6];
+        protected byte m_Model = 0;
+        protected uint m_Packet;
+        protected byte m_PlugStatus = 0;
+        protected bool m_Publish = false;
+        protected ReportEventArgs m_ReportArgs = new ReportEventArgs();
         protected DsState m_State = DsState.Disconnected;
 
-        protected UInt32 m_Packet = 0;
-
-        public virtual DsModel Model 
-        {
-            get { return (DsModel) m_Model; }
-        }
-        
-        public virtual DsPadId PadId 
-        {
-            get { return (DsPadId) m_ControllerId; }
-            set
-            {
-                m_ControllerId = (Byte) value;
-
-                m_ReportArgs.Pad = PadId;
-            }
-        }
-
-        public virtual DsConnection Connection 
-        {
-            get { return DsConnection.USB; }
-        }
-
-        public virtual DsState State 
-        {
-            get { return (DsState) m_State; }
-        }
-
-        public virtual DsBattery Battery 
-        {
-            get { return (DsBattery) m_BatteryStatus; }
-        }
-
-        public virtual Byte[] BD_Address 
-        {
-            get { return m_Local; }
-        }
-
-        public virtual String Local  
-        {
-            get { return m_Mac; }
-        }
-
-        public virtual String Remote 
-        {
-            get { return String.Format("{0:X2}:{1:X2}:{2:X2}:{3:X2}:{4:X2}:{5:X2}", m_Master[0], m_Master[1], m_Master[2], m_Master[3], m_Master[4], m_Master[5]); }
-        }
-
-        public virtual Boolean IsShutdown 
-        {
-            get { return m_IsDisconnect; }
-            set { m_IsDisconnect = value; }
-        }
-        
-        protected virtual void Publish() 
-        {
-            m_ReportArgs.Report[0] = m_ControllerId;
-            m_ReportArgs.Report[1] = (Byte) m_State;
-
-            if (Report != null) Report(this, m_ReportArgs);
-        }
-
-        protected virtual void Process(DateTime now) 
-        {
-        }
-
-        protected virtual void Parse(Byte[] Report) 
-        {
-        }
-
-        protected virtual Boolean Shutdown() 
-        {
-            Stop();
-
-            return RestartDevice(m_Instance);
-        }
-        
-        protected UsbDevice(String Guid) : base(Guid) 
+        protected UsbDevice(string Guid) : base(Guid)
         {
             InitializeComponent();
         }
 
-
-        public UsbDevice() 
+        public UsbDevice()
         {
             InitializeComponent();
         }
 
-        public UsbDevice(IContainer container) 
+        public UsbDevice(IContainer container)
         {
             container.Add(this);
 
             InitializeComponent();
         }
-        
-        public override Boolean Start() 
+
+        public virtual bool IsShutdown
+        {
+            get { return m_IsDisconnect; }
+            set { m_IsDisconnect = value; }
+        }
+
+        public virtual DsModel Model
+        {
+            get { return (DsModel) m_Model; }
+        }
+
+        public virtual DsPadId PadId
+        {
+            get { return (DsPadId) m_ControllerId; }
+            set
+            {
+                m_ControllerId = (byte) value;
+
+                m_ReportArgs.Pad = PadId;
+            }
+        }
+
+        public virtual DsConnection Connection
+        {
+            get { return DsConnection.USB; }
+        }
+
+        public virtual DsState State
+        {
+            get { return m_State; }
+        }
+
+        public virtual DsBattery Battery
+        {
+            get { return (DsBattery) m_BatteryStatus; }
+        }
+
+        public virtual byte[] BD_Address
+        {
+            get { return m_Local; }
+        }
+
+        public virtual string Local
+        {
+            get { return m_Mac; }
+        }
+
+        public virtual string Remote
+        {
+            get
+            {
+                return string.Format("{0:X2}:{1:X2}:{2:X2}:{3:X2}:{4:X2}:{5:X2}", m_Master[0], m_Master[1], m_Master[2],
+                    m_Master[3], m_Master[4], m_Master[5]);
+            }
+        }
+
+        public override bool Start()
         {
             if (IsActive)
             {
-                Array.Copy(m_Local, 0, m_ReportArgs.Report, (Int32) DsOffset.Address, m_Local.Length);
+                Array.Copy(m_Local, 0, m_ReportArgs.Report, (int) DsOffset.Address, m_Local.Length);
 
-                m_ReportArgs.Report[(Int32) DsOffset.Connection] = (Byte) Connection;
-                m_ReportArgs.Report[(Int32) DsOffset.Model     ] = (Byte) Model;
+                m_ReportArgs.Report[(int) DsOffset.Connection] = (byte) Connection;
+                m_ReportArgs.Report[(int) DsOffset.Model] = (byte) Model;
 
-                m_State  = DsState.Connected;
+                m_State = DsState.Connected;
                 m_Packet = 0;
 
                 HID_Worker.RunWorkerAsync();
@@ -143,7 +116,47 @@ namespace ScpControl
             return State == DsState.Connected;
         }
 
-        public override Boolean Stop() 
+        public virtual bool Rumble(byte large, byte small)
+        {
+            return false;
+        }
+
+        public virtual bool Pair(byte[] master)
+        {
+            return false;
+        }
+
+        public virtual bool Disconnect()
+        {
+            return true;
+        }
+
+        public event EventHandler<ReportEventArgs> Report;
+
+        protected virtual void Publish()
+        {
+            m_ReportArgs.Report[0] = m_ControllerId;
+            m_ReportArgs.Report[1] = (byte) m_State;
+
+            if (Report != null) Report(this, m_ReportArgs);
+        }
+
+        protected virtual void Process(DateTime now)
+        {
+        }
+
+        protected virtual void Parse(byte[] Report)
+        {
+        }
+
+        protected virtual bool Shutdown()
+        {
+            Stop();
+
+            return RestartDevice(m_Instance);
+        }
+
+        public override bool Stop()
         {
             if (IsActive)
             {
@@ -156,7 +169,7 @@ namespace ScpControl
             return base.Stop();
         }
 
-        public override Boolean Close() 
+        public override bool Close()
         {
             if (IsActive)
             {
@@ -171,21 +184,21 @@ namespace ScpControl
             return !IsActive;
         }
 
-        public override String ToString() 
+        public override string ToString()
         {
-            switch ((DsState) m_State)
+            switch (m_State)
             {
                 case DsState.Disconnected:
 
-                    return String.Format("Pad {0} : Disconnected", m_ControllerId + 1);
+                    return string.Format("Pad {0} : Disconnected", m_ControllerId + 1);
 
                 case DsState.Reserved:
 
-                    return String.Format("Pad {0} : {1} {2} - Reserved", m_ControllerId + 1, Model, Local);
+                    return string.Format("Pad {0} : {1} {2} - Reserved", m_ControllerId + 1, Model, Local);
 
                 case DsState.Connected:
 
-                    return String.Format("Pad {0} : {1} {2} - {3} {4:X8} {5}", m_ControllerId + 1, Model,
+                    return string.Format("Pad {0} : {1} {2} - {3} {4:X8} {5}", m_ControllerId + 1, Model,
                         Local,
                         Connection,
                         m_Packet,
@@ -195,11 +208,11 @@ namespace ScpControl
 
             throw new Exception();
         }
-        
-        protected void HID_Worker_Thread(object sender, DoWorkEventArgs e) 
+
+        private void HID_Worker_Thread(object sender, DoWorkEventArgs e)
         {
-            Int32  Transfered = 0;
-            Byte[] Buffer = new Byte[64];
+            var transfered = 0;
+            var buffer = new byte[64];
 
             Log.Debug("-- USB Device : HID_Worker_Thread Starting");
 
@@ -207,9 +220,9 @@ namespace ScpControl
             {
                 try
                 {
-                    if (ReadIntPipe(Buffer, Buffer.Length, ref Transfered) && Transfered > 0)
+                    if (ReadIntPipe(buffer, buffer.Length, ref transfered) && transfered > 0)
                     {
-                        Parse(Buffer);
+                        Parse(buffer);
                     }
                 }
                 catch (Exception ex)
@@ -221,27 +234,12 @@ namespace ScpControl
             Log.Debug("-- USB Device : HID_Worker_Thread Exiting");
         }
 
-        protected void On_Timer(object sender, EventArgs e) 
+        private void On_Timer(object sender, EventArgs e)
         {
             lock (this)
             {
                 Process(DateTime.Now);
             }
-        }
-        
-        public virtual Boolean Rumble(Byte large, Byte small) 
-        {
-            return false;
-        }
-
-        public virtual Boolean Pair(Byte[] master) 
-        {
-            return false;
-        }
-
-        public virtual Boolean Disconnect() 
-        {
-            return true;
         }
     }
 }
