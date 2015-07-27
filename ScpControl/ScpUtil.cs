b760a2d5c5d9f06a20233d679d6d17497c5d5ad0
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-27 15:09:12 +0200

    Minor clean-up

--- ScpControl/ScpUtil.cs
+++ ScpControl/ScpUtil.cs
@@ -6,7 +6,6 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Xml;
 using Microsoft.Win32;
