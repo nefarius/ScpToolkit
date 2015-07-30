7794fddd7cb17bcfd40cc9b65ac8d287eb34a1f3
Benjamin Höglinger <nefarius@dhmx.at>
2015-07-30 17:02:56 +0200

    Moved interop code from ScpDevice to it's own file

--- ScpControl/ScpDevice.cs
+++ ScpControl/ScpDevice.cs
@@ -112,6 +112,8 @@ namespace ScpControl
             return Stop();
         }
 
+        #region WinUSB wrapper methods
+
         public virtual bool ReadIntPipe(byte[] Buffer, int Length, ref int Transfered)
         {
             if (!m_IsActive) return false;
@@ -154,6 +156,8 @@ namespace ScpControl
 
             return WinUsb_ControlTransfer(m_WinUsbHandle, Setup, Buffer, Buffer.Length, ref Transfered, IntPtr.Zero);
         }
+
+        #endregion
 
         #region Constant and Structure Definitions
 
@@ -661,107 +665,6 @@ namespace ScpControl
 
             return false;
         }
-
-        #endregion
-
-        #region Interop Definitions
-
-        [DllImport("setupapi.dll", SetLastError = true)]
-        protected static extern int SetupDiCreateDeviceInfoList(ref Guid ClassGuid, int hwndParent);
-
-        [DllImport("setupapi.dll", SetLastError = true)]
-        protected static extern int SetupDiDestroyDeviceInfoList(IntPtr DeviceInfoSet);
-
-        [DllImport("setupapi.dll", SetLastError = true)]
-        protected static extern bool SetupDiEnumDeviceInterfaces(IntPtr DeviceInfoSet, IntPtr DeviceInfoData,
-            ref Guid InterfaceClassGuid, int MemberIndex, ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, IntPtr Enumerator, IntPtr hwndParent,
-            int Flags);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool SetupDiGetDeviceInterfaceDetail(IntPtr DeviceInfoSet,
-            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData, IntPtr DeviceInterfaceDetailData,
-            int DeviceInterfaceDetailDataSize, ref int RequiredSize, IntPtr DeviceInfoData);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool SetupDiGetDeviceInterfaceDetail(IntPtr DeviceInfoSet,
-            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData, IntPtr DeviceInterfaceDetailData,
-            int DeviceInterfaceDetailDataSize, ref int RequiredSize, ref SP_DEVICE_INTERFACE_DATA DeviceInfoData);
-
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-        protected static extern IntPtr RegisterDeviceNotification(IntPtr hRecipient, IntPtr NotificationFilter,
-            int Flags);
-
-        [DllImport("user32.dll", SetLastError = true)]
-        protected static extern bool UnregisterDeviceNotification(IntPtr Handle);
-
-        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern IntPtr CreateFile(string lpFileName, uint dwDesiredAccess, uint dwShareMode,
-            IntPtr lpSecurityAttributes, uint dwCreationDisposition, uint dwFlagsAndAttributes, uint hTemplateFile);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_Initialize(IntPtr DeviceHandle, ref IntPtr InterfaceHandle);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_QueryInterfaceSettings(IntPtr InterfaceHandle, byte AlternateInterfaceNumber,
-            ref USB_INTERFACE_DESCRIPTOR UsbAltInterfaceDescriptor);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_QueryPipe(IntPtr InterfaceHandle, byte AlternateInterfaceNumber,
-            byte PipeIndex, ref WINUSB_PIPE_INFORMATION PipeInformation);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_AbortPipe(IntPtr InterfaceHandle, byte PipeID);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_FlushPipe(IntPtr InterfaceHandle, byte PipeID);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_ControlTransfer(IntPtr InterfaceHandle, WINUSB_SETUP_PACKET SetupPacket,
-            byte[] Buffer, int BufferLength, ref int LengthTransferred, IntPtr Overlapped);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_ReadPipe(IntPtr InterfaceHandle, byte PipeID, byte[] Buffer,
-            int BufferLength, ref int LengthTransferred, IntPtr Overlapped);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_WritePipe(IntPtr InterfaceHandle, byte PipeID, byte[] Buffer,
-            int BufferLength, ref int LengthTransferred, IntPtr Overlapped);
-
-        [DllImport("winusb.dll", SetLastError = true)]
-        protected static extern bool WinUsb_Free(IntPtr InterfaceHandle);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        public static extern IntPtr RegisterServiceCtrlHandlerEx(string ServiceName, ServiceControlHandlerEx Callback,
-            IntPtr Context);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        protected static extern bool DeviceIoControl(IntPtr DeviceHandle, int IoControlCode, byte[] InBuffer,
-            int InBufferSize, byte[] OutBuffer, int OutBufferSize, ref int BytesReturned, IntPtr Overlapped);
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        protected static extern bool CloseHandle(IntPtr Handle);
-
-        [DllImport("kernel32.dll")]
-        protected static extern int GetLastError();
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern int CM_Get_Device_ID(int dnDevInst, IntPtr Buffer, int BufferLen, int ulFlags);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool SetupDiOpenDeviceInfo(IntPtr DeviceInfoSet, string DeviceInstanceId,
-            IntPtr hwndParent, int Flags, ref SP_DEVICE_INTERFACE_DATA DeviceInfoData);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool SetupDiChangeState(IntPtr DeviceInfoSet,
-            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData);
-
-        [DllImport("setupapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        protected static extern bool SetupDiSetClassInstallParams(IntPtr DeviceInfoSet,
-            ref SP_DEVICE_INTERFACE_DATA DeviceInterfaceData, ref SP_PROPCHANGE_PARAMS ClassInstallParams,
-            int ClassInstallParamsSize);
 
         #endregion
     }
