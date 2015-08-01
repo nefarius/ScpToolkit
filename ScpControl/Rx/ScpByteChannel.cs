e4ad1486473d12b8ca2027513079fcfbf5f68fb1
Benjamin Höglinger <nefarius@dhmx.at>
2015-08-01 22:18:53 +0200

    Improvements

--- ScpControl/Rx/ScpByteChannel.cs
+++ ScpControl/Rx/ScpByteChannel.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
-using log4net;
-using log4net.Repository.Hierarchy;
 using ReactiveSockets;
 
 namespace ScpControl.Rx
@@ -23,7 +19,7 @@ namespace ScpControl.Rx
         GetXml = 0x08,
         SetXml = 0x09,
         PadDetail = 0x0A,
-        NativeFeedAvailable = 0x0B,
+        NativeFeedAvailable = 0x0B
     }
 
     public interface IScpPacket<T>
@@ -39,27 +35,26 @@ namespace ScpControl.Rx
     }
 
     /// <summary>
-    /// Implements a communication channel over a socket that 
-    /// has a fixed length header and a variable length string 
-    /// payload.
+    ///     Implements a communication channel over a socket that
+    ///     has a fixed length header and a variable length string
+    ///     payload.
     /// </summary>
     public class ScpByteChannel : IChannel<ScpBytePacket>
     {
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         private readonly IReactiveSocket _socket;
 
         /// <summary>
-        /// Initializes the channel with the given socket, using 
-        /// the given encoding for messages.
+        ///     Initializes the channel with the given socket, using
+        ///     the given encoding for messages.
         /// </summary>
         public ScpByteChannel(IReactiveSocket socket)
         {
-            this._socket = socket;
+            _socket = socket;
 
-            Receiver = from header in socket.Receiver.Buffer(sizeof(int))
-                       let length = BitConverter.ToInt32(header.ToArray(), 0)
-                       let body = socket.Receiver.Take(length).ToEnumerable().ToArray()
-                       select new ScpBytePacket() { Request = (ScpRequest)body[0], Payload = body.Skip(1).ToArray() };
+            Receiver = from header in socket.Receiver.Buffer(sizeof (int))
+                let length = BitConverter.ToInt32(header.ToArray(), 0)
+                let body = socket.Receiver.Take(length).ToEnumerable().ToArray()
+                select new ScpBytePacket {Request = (ScpRequest) body[0], Payload = body.Skip(1).ToArray()};
         }
 
         public IObservable<ScpBytePacket> Receiver { get; private set; }
@@ -78,20 +73,33 @@ namespace ScpControl.Rx
 
         public Task SendAsync(ScpRequest request, byte[] payload)
         {
-            return SendAsync(new ScpBytePacket() { Request = request, Payload = payload });
+            return SendAsync(new ScpBytePacket {Request = request, Payload = payload});
         }
 
         public Task SendAsync(ScpRequest request)
         {
-            return SendAsync(new ScpBytePacket() { Request = request, Payload = new[] { (byte)0x00, (byte)0x00 } });
+            return SendAsync(new ScpBytePacket {Request = request});
         }
 
         private static byte[] Convert(ScpBytePacket message)
         {
-            var body = message.Payload;
-            var header = BitConverter.GetBytes(body.Length + sizeof(byte)).ToList();
-            header.Add((byte)message.Request);
-            var payload = header.Concat(body).ToArray();
+            byte[] payload;
+
+            if (message.Payload != null)
+            {
+                // build buffer with length, request and payload
+                var body = message.Payload;
+                var header = BitConverter.GetBytes(body.Length + sizeof (byte)).ToList();
+                header.Add((byte) message.Request);
+                payload = header.Concat(body).ToArray();
+            }
+            else
+            {
+                // build buffer with length and request only
+                var header = BitConverter.GetBytes(sizeof (byte)).ToList();
+                header.Add((byte) message.Request);
+                payload = header.ToArray();
+            }
 
             return payload;
         }
