3add9370f290c5a7ae46e92b584b71245e115149
Benjamin Höglinger <nefarius@dhmx.at>
2015-08-08 21:53:24 +0200

    Added missing files

---
+++ ScpControl/ScpCore/SingletonBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ScpControl.ScpCore
+{
+    /// <summary>
+    /// A base class for the singleton design pattern.
+    /// </summary>
+    /// <typeparam name="T">Class type of the singleton</typeparam>
+    public abstract class SingletonBase<T> where T : class
+    {
+        #region Members
+
+        /// <summary>
+        /// Static instance. Needs to use lambda expression
+        /// to construct an instance (since constructor is private).
+        /// </summary>
+        private static readonly Lazy<T> SInstance = new Lazy<T>(CreateInstanceOfT);
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the instance of this singleton.
+        /// </summary>
+        public static T Instance { get { return SInstance.Value; } }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Creates an instance of T via reflection since T's constructor is expected to be private.
+        /// </summary>
+        /// <returns></returns>
+        private static T CreateInstanceOfT()
+        {
+            return Activator.CreateInstance(typeof(T), true) as T;
+        }
+
+        #endregion
+    }
+}
---
+++ ScpControl/UsbDs3AfterglowAp2.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Threading;
+
+namespace ScpControl
+{
+    public partial class UsbDs3AfterglowAp2 : UsbDs3
+    {
+        public UsbDs3AfterglowAp2()
+        {
+            InitializeComponent();
+        }
+
+        public UsbDs3AfterglowAp2(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        protected override void Parse(byte[] Report)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var b in Report)
+            {
+                sb.AppendFormat("{0:X2} ", b);
+            }
+
+            Log.DebugFormat("Packet: {0}", sb);
+            Thread.Sleep(1000);
+
+            base.Parse(Report);
+        }
+    }
+}
