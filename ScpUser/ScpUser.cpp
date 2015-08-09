1e1e504125061d15018732eb455eed5f0fcd10e4
Benjamin Höglinger <nefarius@dhmx.at>
2015-08-09 12:49:27 +0200

    Removed BugTrap

--- ScpUser/ScpUser.cpp
+++ ScpUser/ScpUser.cpp
@@ -8,8 +8,6 @@ using namespace SCPUser;
 [STAThreadAttribute]
 int main(array<System::String ^> ^args)
 {
-	SetupExceptionHandler();
-
 	// Enabling Windows XP visual effects before any controls are created
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false); 
--- ScpUser/stdafx.h
+++ ScpUser/stdafx.h
@@ -10,24 +10,3 @@
 #include "Resource.h"
 
 #define XINPUT_GAMEPAD_GUIDE 0x400
-
-#include "BugTrap\BugTrap.h"
-#include "BugTrap\BTTrace.h"
-
-#ifdef _M_AMD64
-#pragma comment(lib, "BugTrap\\BugTrapU-x64.lib")
-#else
-#pragma comment(lib, "BugTrap\\BugTrapU.lib")
-#endif
-
-namespace SCPUser 
-{
-	static void SetupExceptionHandler()
-	{
-		BT_SetAppName(_T("XInput Controller Tester"));
-		//BT_SetSupportEMail(_T("your@email.com"));
-		BT_SetFlags(BTF_DETAILEDMODE);
-		//BT_SetSupportServer(_T("localhost"), 9999);
-		BT_SetSupportURL(_T("https://github.com/nefarius/ScpServer"));
-	}
-}
