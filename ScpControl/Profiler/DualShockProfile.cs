198e415c198a64c0654a25c6e3c874be3690136b
Benjamin Höglinger <nefarius@dhmx.at>
2015-08-28 17:45:01 +0200

    Ported over Ds4 button definitions

--- ScpControl/Profiler/DualShockProfile.cs
+++ ScpControl/Profiler/DualShockProfile.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 using ScpControl.ScpCore;
 
 namespace ScpControl.Profiler
@@ -24,7 +20,6 @@ namespace ScpControl.Profiler
     public class ButtonMappingTarget
     {
         public CommandType CommandType { get; set; }
-
         public object CommandTarget { get; set; }
     }
 
@@ -39,143 +34,525 @@ namespace ScpControl.Profiler
 
     public interface IDsButton
     {
-
+        uint RawValue { get; }
+        string Name { get; }
+        string DisplayName { get; }
     }
 
     public class DsButton : IDsButton
     {
-        public uint RawValue { get; protected set; }
-        public string Name { get; protected set; }
-        public string DisplayName { get; protected set; }
-
         public DsButton()
-        { }
+        {
+        }
 
         public DsButton(string name)
             : this()
         {
             Name = name;
         }
+
+        public uint RawValue { get; protected set; }
+        public string Name { get; private set; }
+        public string DisplayName { get; protected set; }
     }
 
+    /// <summary>
+    ///     Definition of a DualShock 3 button.
+    /// </summary>
     public class Ds3Button : DsButton
     {
-        public Ds3Button() { }
+        #region Ctors
+
+        public Ds3Button()
+        {
+        }
 
         public Ds3Button(string name)
             : base(name)
-        { }
+        {
+        }
 
-        public static IList<IDsButton> Buttons
+        #endregion
+
+        #region Buttons
+
+        public static IDsButton None
         {
             get
             {
-                return new List<IDsButton>
+                return new Ds3Button("None")
                 {
-                    new Ds3Button("None")
-                    {
-                        RawValue = 0,
-                        DisplayName = "None"
-                    },
-                    new Ds3Button("Select")
-                    {
-                        RawValue = 1 << 0,
-                        DisplayName = "Select"
-                    },
-                    new Ds3Button("L3")
-                    {
-                        RawValue = 1 << 1,
-                        DisplayName = "Left thumb"
-                    },
-                    new Ds3Button("R3")
-                    {
-                        RawValue = 1 << 2,
-                        DisplayName = "Right thumb"
-                    },
-                    new Ds3Button("Start")
-                    {
-                        RawValue = 1 << 3,
-                        DisplayName = "Start"
-                    },
-                    new Ds3Button("Up")
-                    {
-                        RawValue = 1 << 4,
-                        DisplayName = "D-Pad up"
-                    },
-                    new Ds3Button("Right")
-                    {
-                        RawValue = 1 << 5,
-                        DisplayName = "D-Pad right"
-                    },
-                    new Ds3Button("Down")
-                    {
-                        RawValue = 1 << 6,
-                        DisplayName = "D-Pad down"
-                    },
-                    new Ds3Button("Left")
-                    {
-                        RawValue = 1 << 7,
-                        DisplayName = "D-Pad left"
-                    },
-                    new Ds3Button("L2")
-                    {
-                        RawValue = 1 << 8,
-                        DisplayName = "Left trigger"
-                    },
-                    new Ds3Button("R2")
-                    {
-                        RawValue = 1 << 9,
-                        DisplayName = "Right trigger"
-                    },
-                    new Ds3Button("L1")
-                    {
-                        RawValue = 1 << 10,
-                        DisplayName = "Left shoulder"
-                    },
-                    new Ds3Button("R1")
-                    {
-                        RawValue = 1 << 11,
-                        DisplayName = "Right shoulder"
-                    },
-                    new Ds3Button("Triangle")
-                    {
-                        RawValue = 1 << 12,
-                        DisplayName = "Triangle"
-                    },
-                    new Ds3Button("Circle")
-                    {
-                        RawValue = 1 << 13,
-                        DisplayName = "Circle"
-                    },
-                    new Ds3Button("Cross")
-                    {
-                        RawValue = 1 << 14,
-                        DisplayName = "Cross"
-                    },
-                    new Ds3Button("Square")
-                    {
-                        RawValue = 1 << 15,
-                        DisplayName = "Square"
-                    },
-                    new Ds3Button("PS")
-                    {
-                        RawValue = 1 << 16,
-                        DisplayName = "PS"
-                    }
+                    RawValue = 0,
+                    DisplayName = "None"
                 };
             }
         }
+
+        public static IDsButton Select
+        {
+            get
+            {
+                return new Ds3Button("Select")
+                {
+                    RawValue = 1 << 0,
+                    DisplayName = "Select"
+                };
+            }
+        }
+
+        public static IDsButton L3
+        {
+            get
+            {
+                return new Ds3Button("L3")
+                {
+                    RawValue = 1 << 1,
+                    DisplayName = "Left thumb"
+                };
+            }
+        }
+
+        public static IDsButton R3
+        {
+            get
+            {
+                return new Ds3Button("R3")
+                {
+                    RawValue = 1 << 2,
+                    DisplayName = "Right thumb"
+                };
+            }
+        }
+
+        public static IDsButton Start
+        {
+            get
+            {
+                return new Ds3Button("Start")
+                {
+                    RawValue = 1 << 3,
+                    DisplayName = "Start"
+                };
+            }
+        }
+
+        public static IDsButton Up
+        {
+            get
+            {
+                return new Ds3Button("Up")
+                {
+                    RawValue = 1 << 4,
+                    DisplayName = "D-Pad up"
+                };
+            }
+        }
+
+        public static IDsButton Right
+        {
+            get
+            {
+                return new Ds3Button("Right")
+                {
+                    RawValue = 1 << 5,
+                    DisplayName = "D-Pad right"
+                };
+            }
+        }
+
+        public static IDsButton Down
+        {
+            get
+            {
+                return new Ds3Button("Down")
+                {
+                    RawValue = 1 << 6,
+                    DisplayName = "D-Pad down"
+                };
+            }
+        }
+
+        public static IDsButton Left
+        {
+            get
+            {
+                return new Ds3Button("Left")
+                {
+                    RawValue = 1 << 7,
+                    DisplayName = "D-Pad left"
+                };
+            }
+        }
+
+        public static IDsButton L2
+        {
+            get
+            {
+                return new Ds3Button("L2")
+                {
+                    RawValue = 1 << 8,
+                    DisplayName = "Left trigger"
+                };
+            }
+        }
+
+        public static IDsButton R2
+        {
+            get
+            {
+                return new Ds3Button("R2")
+                {
+                    RawValue = 1 << 9,
+                    DisplayName = "Right trigger"
+                };
+            }
+        }
+
+        public static IDsButton L1
+        {
+            get
+            {
+                return new Ds3Button("L1")
+                {
+                    RawValue = 1 << 10,
+                    DisplayName = "Left shoulder"
+                };
+            }
+        }
+
+        public static IDsButton R1
+        {
+            get
+            {
+                return new Ds3Button("R1")
+                {
+                    RawValue = 1 << 11,
+                    DisplayName = "Right shoulder"
+                };
+            }
+        }
+
+        public static IDsButton Triangle
+        {
+            get
+            {
+                return new Ds3Button("Triangle")
+                {
+                    RawValue = 1 << 12,
+                    DisplayName = "Triangle"
+                };
+            }
+        }
+
+        public static IDsButton Circle
+        {
+            get
+            {
+                return new Ds3Button("Circle")
+                {
+                    RawValue = 1 << 13,
+                    DisplayName = "Circle"
+                };
+            }
+        }
+
+        public static IDsButton Cross
+        {
+            get
+            {
+                return new Ds3Button("Cross")
+                {
+                    RawValue = 1 << 14,
+                    DisplayName = "Cross"
+                };
+            }
+        }
+
+        public static IDsButton Square
+        {
+            get
+            {
+                return new Ds3Button("Square")
+                {
+                    RawValue = 1 << 15,
+                    DisplayName = "Square"
+                };
+            }
+        }
+
+        public static IDsButton Ps
+        {
+            get
+            {
+                return new Ds3Button("PS")
+                {
+                    RawValue = 1 << 16,
+                    DisplayName = "PS"
+                };
+            }
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    ///     Definition of a DualShock 4 button.
+    /// </summary>
+    public class Ds4Button : DsButton
+    {
+        #region Ctors
+
+        public Ds4Button()
+        {
+        }
+
+        public Ds4Button(string name)
+            : base(name)
+        {
+        }
+
+        #endregion
+
+        #region Buttons
+
+        public static IDsButton None
+        {
+            get
+            {
+                return new Ds4Button("None")
+                {
+                    RawValue = 0,
+                    DisplayName = "None"
+                };
+            }
+        }
+
+        public static IDsButton Up
+        {
+            get
+            {
+                return new Ds4Button("Up")
+                {
+                    RawValue = 1 << 0,
+                    DisplayName = "D-Pad up"
+                };
+            }
+        }
+
+        public static IDsButton Right
+        {
+            get
+            {
+                return new Ds4Button("Right")
+                {
+                    RawValue = 1 << 1,
+                    DisplayName = "D-Pad right"
+                };
+            }
+        }
+
+        public static IDsButton Down
+        {
+            get
+            {
+                return new Ds4Button("Down")
+                {
+                    RawValue = 1 << 2,
+                    DisplayName = "D-Pad down"
+                };
+            }
+        }
+
+        public static IDsButton Left
+        {
+            get
+            {
+                return new Ds4Button("Left")
+                {
+                    RawValue = 1 << 3,
+                    DisplayName = "D-Pad left"
+                };
+            }
+        }
+
+        public static IDsButton Square
+        {
+            get
+            {
+                return new Ds4Button("Square")
+                {
+                    RawValue = 1 << 4,
+                    DisplayName = "Square"
+                };
+            }
+        }
+
+        public static IDsButton Cross
+        {
+            get
+            {
+                return new Ds4Button("Cross")
+                {
+                    RawValue = 1 << 5,
+                    DisplayName = "Cross"
+                };
+            }
+        }
+
+        public static IDsButton Circle
+        {
+            get
+            {
+                return new Ds4Button("Circle")
+                {
+                    RawValue = 1 << 6,
+                    DisplayName = "Circle"
+                };
+            }
+        }
+
+        public static IDsButton Triangle
+        {
+            get
+            {
+                return new Ds4Button("Triangle")
+                {
+                    RawValue = 1 << 7,
+                    DisplayName = "Triangle"
+                };
+            }
+        }
+
+        public static IDsButton L1
+        {
+            get
+            {
+                return new Ds4Button("L1")
+                {
+                    RawValue = 1 << 8,
+                    DisplayName = "Left shoulder"
+                };
+            }
+        }
+
+        public static IDsButton R1
+        {
+            get
+            {
+                return new Ds4Button("R1")
+                {
+                    RawValue = 1 << 9,
+                    DisplayName = "Right shoulder"
+                };
+            }
+        }
+
+        public static IDsButton L2
+        {
+            get
+            {
+                return new Ds4Button("L2")
+                {
+                    RawValue = 1 << 10,
+                    DisplayName = "Left trigger"
+                };
+            }
+        }
+
+        public static IDsButton R2
+        {
+            get
+            {
+                return new Ds4Button("R2")
+                {
+                    RawValue = 1 << 11,
+                    DisplayName = "Right trigger"
+                };
+            }
+        }
+
+        public static IDsButton Share
+        {
+            get
+            {
+                return new Ds4Button("Share")
+                {
+                    RawValue = 1 << 12,
+                    DisplayName = "Share"
+                };
+            }
+        }
+
+        public static IDsButton Options
+        {
+            get
+            {
+                return new Ds4Button("Options")
+                {
+                    RawValue = 1 << 13,
+                    DisplayName = "Options"
+                };
+            }
+        }
+
+        public static IDsButton L3
+        {
+            get
+            {
+                return new Ds4Button("L3")
+                {
+                    RawValue = 1 << 14,
+                    DisplayName = "Left thumb"
+                };
+            }
+        }
+
+        public static IDsButton R3
+        {
+            get
+            {
+                return new Ds4Button("R3")
+                {
+                    RawValue = 1 << 15,
+                    DisplayName = "Right thumb"
+                };
+            }
+        }
+
+        public static IDsButton Ps
+        {
+            get
+            {
+                return new Ds4Button("PS")
+                {
+                    RawValue = 1 << 16,
+                    DisplayName = "PS"
+                };
+            }
+        }
+
+        public static IDsButton TouchPad
+        {
+            get
+            {
+                return new Ds4Button("TouchPad")
+                {
+                    RawValue = 1 << 17,
+                    DisplayName = "Touchpad"
+                };
+            }
+        }
+
+        #endregion
     }
 
     public class DualShockProfile
     {
-        public DsButtonMaps<IDsButton> ButtonMaps { get; set; }
-
         public DualShockProfile()
         {
             ButtonMaps = new DsButtonMaps<IDsButton>();
-
-            
         }
+
+        public DsButtonMaps<IDsButton> ButtonMaps { get; set; }
     }
 }
