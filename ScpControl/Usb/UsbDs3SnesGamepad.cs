f90173da92d9ecdddeff5d44665222a8c44f3f7a
Benjamin Höglinger <nefarius@dhmx.at>
2015-09-28 19:01:09 +0200

    Overriding unsupported methods for SNES Gamepad

--- ScpControl/Usb/UsbDs3SnesGamepad.cs
+++ ScpControl/Usb/UsbDs3SnesGamepad.cs
@@ -11,5 +11,24 @@ namespace ScpControl.Usb
     /// </summary>
     public class UsbDs3SnesGamepad : UsbDs3
     {
+        protected override void Process(DateTime now)
+        {
+            // ignore
+        }
+
+        public override bool Pair(byte[] master)
+        {
+            return false; // ignore
+        }
+
+        public override bool Rumble(byte large, byte small)
+        {
+            return false; // ignore
+        }
+
+        protected override void Parse(byte[] report)
+        {
+            base.Parse(report);
+        }
     }
 }
