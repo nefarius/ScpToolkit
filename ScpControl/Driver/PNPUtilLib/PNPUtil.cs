c5b32818baf8645397d5c69faa2d661c9043a28c
Benjamin Höglinger <nefarius@dhmx.at>
2015-09-30 19:09:10 +0200

    Performed code clean-up

--- ScpControl/Driver/PNPUtilLib/PNPUtil.cs
+++ ScpControl/Driver/PNPUtilLib/PNPUtil.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using log4net;
 
 /*** 
@@ -11,107 +12,28 @@ using log4net;
  * Dec 30, 2012 : Merged in user submitted patch (ID: 10344) to support non-English builds. Thanks jenda__. 
  * 
  */
+
 namespace ScpControl.Driver.PNPUtilLib
 {
-
     public class PnpUtil : IDriverStore
     {
         private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        enum PnpUtilOptions { Enumerate, Delete, ForceDelete, Add, AddInstall };
-        #region IDriverStore Members
-        public List<DriverStoreEntry> EnumeratePackages()
+        private static bool PnpUtilHelper(PnpUtilOptions option, string infName, ref string output)
         {
-            List<DriverStoreEntry> ldse = new List<DriverStoreEntry>();
-            string output = ""; 
-
-            bool result = PnpUtilHelper(PnpUtilOptions.Enumerate, "",  ref output);
-            if (result == true)
-            {                
-                // Trace.TraceInformation("O/P of Enumeration : " + Environment.NewLine + output + Environment.NewLine);
-
-                // Parse the output
-                // [jenda_] Didn't work on non-english Windows - changed from string recognition to counting lines
-                using (StringReader sr = new StringReader(output))                
-                {
-                    string currentLine = "";
-                    string[] currentLineDivided = { };
-                    DriverStoreEntry dse = new DriverStoreEntry();
-                    byte lineNum = 0;
-                    while ((currentLine = sr.ReadLine()) != null)
-                    {
-                        currentLineDivided = currentLine.Split(new char[] { ':' });
-                        if (currentLineDivided.Length == 2)
-                        {
-                            currentLineDivided[1] = currentLineDivided[1].Trim();
-                            switch (lineNum)
-                            {
-                                case 0:     // [jenda_] Published name :
-                                    dse.DriverPublishedName = currentLineDivided[1];
-                                    break;
-                                case 1:     //Driver package provider :
-                                    dse.DriverPkgProvider = currentLine.Split(new char[] { ':' })[1].Trim();
-                                    break;
-                                case 2:     // [jenda_] Class :
-                                    dse.DriverClass = currentLine.Split(new char[] { ':' })[1].Trim();
-                                    break;
-                                case 3:     // [jenda_] Driver date and version :
-                                    string DateAndVersion = currentLine.Split(new char[] { ':' })[1].Trim();
-                                    dse.DriverDate = DateAndVersion.Split(new char[] { ' ' })[0].Trim();
-                                    dse.DriverVersion = DateAndVersion.Split(new char[] { ' ' })[1].Trim();
-                                    break;
-                                case 4:     // [jenda_] Signer name :
-                                    dse.DriverSignerName = currentLine.Split(new char[] { ':' })[1].Trim();
-
-                                    ldse.Add(dse);
-                                    dse = new DriverStoreEntry();
-                                    break;
-                                default:
-                                    continue;
-                            }
-                            lineNum++;
-                            if (lineNum > 4)
-                                lineNum = 0;
-
-                        }
-                    }
-                }
-
-            }
-            return ldse;
-        }
-
-        public bool DeletePackage(DriverStoreEntry dse, bool forceDelete)
-        {
-            string dummy = "";
-            return PnpUtilHelper(forceDelete == true ? PnpUtilOptions.ForceDelete : PnpUtilOptions.Delete,
-                          dse.DriverPublishedName, 
-                          ref dummy);
-        }
-
-        public bool AddPackage(string infFullPath, bool install)
-        {
-            string dummy = "";
-            return PnpUtilHelper(install == true ? PnpUtilOptions.AddInstall : PnpUtilOptions.Add,
-                    infFullPath, ref dummy);
-        }
-        #endregion
-
-        static bool PnpUtilHelper(PnpUtilOptions option, string infName, ref string output)
-        {
-            bool retVal = true;
-            bool fDebugPrintOutput = false;
+            var retVal = true;
+            var fDebugPrintOutput = false;
             //
             // Setup the process with the ProcessStartInfo class.
             //
-            ProcessStartInfo start = new ProcessStartInfo
-                                     {
-                                         FileName = @"pnputil.exe" /* exe name.*/,
-                                         UseShellExecute = false,
-                                         RedirectStandardOutput = true,
-                                         CreateNoWindow = true,
-                                         WindowStyle = ProcessWindowStyle.Hidden
-                                     };
+            var start = new ProcessStartInfo
+            {
+                FileName = @"pnputil.exe" /* exe name.*/,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
             switch (option)
             {
                 //
@@ -144,31 +66,30 @@ namespace ScpControl.Driver.PNPUtilLib
             }
 
 
-
-
             //
             // Start the process.
             //
-            string result = "";
+            var result = "";
             try
             {
-                using (Process process = Process.Start(start))
+                using (var process = Process.Start(start))
                 {
                     //
                     // Read in all the text from the process with the StreamReader.
                     //
-                    using (StreamReader reader = process.StandardOutput)
+                    using (var reader = process.StandardOutput)
                     {
                         result = reader.ReadToEnd();
                         output = result;
-                        if (fDebugPrintOutput == true)
-                            Log.DebugFormat("[Result_start] ---- {0}{1}[----- Result_End]{0}", Environment.NewLine, result);
+                        if (fDebugPrintOutput)
+                            Log.DebugFormat("[Result_start] ---- {0}{1}[----- Result_End]{0}", Environment.NewLine,
+                                result);
 
                         if (option == PnpUtilOptions.Delete || option == PnpUtilOptions.ForceDelete)
                         {
                             // [jenda_] Really don't know, how to recognize error without language-specific string recognition :(
                             // [jenda_] But those errors should contain ":"
-                            if (output.Contains(@":"))     //"Deleting the driver package failed"
+                            if (output.Contains(@":")) //"Deleting the driver package failed"
                             {
                                 retVal = false;
                             }
@@ -187,12 +108,14 @@ namespace ScpControl.Driver.PNPUtilLib
                              * blah blah blah:    *number*
                              * 
                              */
-                            System.Text.RegularExpressions.Match MatchResult = System.Text.RegularExpressions.Regex.Match(output, @".+: +([0-9]+)[\r\n].+: +([0-9]+)[\r\n ]+", System.Text.RegularExpressions.RegexOptions.Singleline);
+                            var MatchResult = Regex.Match(output, @".+: +([0-9]+)[\r\n].+: +([0-9]+)[\r\n ]+",
+                                RegexOptions.Singleline);
 
-                            if (MatchResult.Success)    // [jenda_] regex recognized successfully
+                            if (MatchResult.Success) // [jenda_] regex recognized successfully
                             {
                                 // [jenda_] if trying to add "0" packages or if number packages and number added packages differs
-                                if (MatchResult.Groups[1].Value == "0" || MatchResult.Groups[1].Value != MatchResult.Groups[2].Value)
+                                if (MatchResult.Groups[1].Value == "0" ||
+                                    MatchResult.Groups[1].Value != MatchResult.Groups[2].Value)
                                 {
                                     Log.ErrorFormat("[Error] failed to add " + infName);
                                     retVal = false;
@@ -202,7 +125,7 @@ namespace ScpControl.Driver.PNPUtilLib
                             {
                                 Log.ErrorFormat("[Error] unknown response while trying to add " + infName);
                                 retVal = false;
-                            }                      
+                            }
                         }
                     }
                 }
@@ -215,6 +138,93 @@ namespace ScpControl.Driver.PNPUtilLib
                 retVal = false;
             }
             return retVal;
-        }        
+        }
+
+        private enum PnpUtilOptions
+        {
+            Enumerate,
+            Delete,
+            ForceDelete,
+            Add,
+            AddInstall
+        };
+
+        #region IDriverStore Members
+
+        public List<DriverStoreEntry> EnumeratePackages()
+        {
+            var ldse = new List<DriverStoreEntry>();
+            var output = "";
+
+            var result = PnpUtilHelper(PnpUtilOptions.Enumerate, "", ref output);
+            if (result)
+            {
+                // Trace.TraceInformation("O/P of Enumeration : " + Environment.NewLine + output + Environment.NewLine);
+
+                // Parse the output
+                // [jenda_] Didn't work on non-english Windows - changed from string recognition to counting lines
+                using (var sr = new StringReader(output))
+                {
+                    var currentLine = "";
+                    string[] currentLineDivided = {};
+                    var dse = new DriverStoreEntry();
+                    byte lineNum = 0;
+                    while ((currentLine = sr.ReadLine()) != null)
+                    {
+                        currentLineDivided = currentLine.Split(':');
+                        if (currentLineDivided.Length == 2)
+                        {
+                            currentLineDivided[1] = currentLineDivided[1].Trim();
+                            switch (lineNum)
+                            {
+                                case 0: // [jenda_] Published name :
+                                    dse.DriverPublishedName = currentLineDivided[1];
+                                    break;
+                                case 1: //Driver package provider :
+                                    dse.DriverPkgProvider = currentLine.Split(':')[1].Trim();
+                                    break;
+                                case 2: // [jenda_] Class :
+                                    dse.DriverClass = currentLine.Split(':')[1].Trim();
+                                    break;
+                                case 3: // [jenda_] Driver date and version :
+                                    var DateAndVersion = currentLine.Split(':')[1].Trim();
+                                    dse.DriverDate = DateAndVersion.Split(' ')[0].Trim();
+                                    dse.DriverVersion = DateAndVersion.Split(' ')[1].Trim();
+                                    break;
+                                case 4: // [jenda_] Signer name :
+                                    dse.DriverSignerName = currentLine.Split(':')[1].Trim();
+
+                                    ldse.Add(dse);
+                                    dse = new DriverStoreEntry();
+                                    break;
+                                default:
+                                    continue;
+                            }
+                            lineNum++;
+                            if (lineNum > 4)
+                                lineNum = 0;
+                        }
+                    }
+                }
+            }
+            return ldse;
+        }
+
+        public bool DeletePackage(DriverStoreEntry dse, bool forceDelete)
+        {
+            var dummy = "";
+            return PnpUtilHelper(forceDelete ? PnpUtilOptions.ForceDelete : PnpUtilOptions.Delete,
+                dse.DriverPublishedName,
+                ref dummy);
+        }
+
+        public bool AddPackage(string infFullPath, bool install)
+        {
+            var dummy = "";
+            return PnpUtilHelper(install ? PnpUtilOptions.AddInstall : PnpUtilOptions.Add,
+                infFullPath, ref dummy);
+        }
+
+        #endregion
     }
 }
