29caf12a9f1468caaaf767ff72391088a87fd4f4
Benjamin Höglinger <nefarius@dhmx.at>
2015-10-21 22:58:24 +0200

    Minor fix

--- ScpControl/Usb/Gamepads/UsbBadBigBenGamepad.cs
+++ ScpControl/Usb/Gamepads/UsbBadBigBenGamepad.cs
@@ -6,7 +6,7 @@ namespace ScpControl.Usb.Gamepads
     {
         protected override void ParseHidReport(byte[] report)
         {
-            if (report[4] != 0x00) return;
+            if (report[5] != 0x00) return;
 
             if (m_Packet++ + 1 < m_Packet)
             {
@@ -25,26 +25,26 @@ namespace ScpControl.Usb.Gamepads
             // packet counter
             m_ReportArgs.SetPacketCounter(m_Packet);
 
-            m_ReportArgs.SetCircleDigital(report[5] >> 5);
-            m_ReportArgs.SetCrossDigital(report[5] >> 6);
-            m_ReportArgs.SetTriangleDigital(report[5] >> 4);
-            m_ReportArgs.SetSquareDigital(report[5] >> 7);
+            m_ReportArgs.SetCircleDigital(report[6] >> 5);
+            m_ReportArgs.SetCrossDigital(report[6] >> 6);
+            m_ReportArgs.SetTriangleDigital(report[6] >> 4);
+            m_ReportArgs.SetSquareDigital(report[6] >> 7);
 
-            m_ReportArgs.SetSelect(report[6] >> 4);
-            m_ReportArgs.SetStart(report[6] >> 5);
+            m_ReportArgs.SetSelect(report[7] >> 4);
+            m_ReportArgs.SetStart(report[7] >> 5);
 
-            m_ReportArgs.SetDpadUpDigital(report[3] == 0x00);
-            m_ReportArgs.SetDpadRightDigital(report[2] == 0xFF);
-            m_ReportArgs.SetDpadDownDigital(report[3] == 0xFF);
-            m_ReportArgs.SetDpadLeftDigital(report[2] == 0x00);
+            m_ReportArgs.SetDpadUpDigital(report[4] == 0x00);
+            m_ReportArgs.SetDpadRightDigital(report[3] == 0xFF);
+            m_ReportArgs.SetDpadDownDigital(report[4] == 0xFF);
+            m_ReportArgs.SetDpadLeftDigital(report[3] == 0x00);
 
-            m_ReportArgs.SetLeftShoulderDigital(report[6] >> 0);
-            m_ReportArgs.SetRightShoulderDigital(report[6] >> 1);
-            m_ReportArgs.SetLeftTriggerDigital(report[6] >> 2);
-            m_ReportArgs.SetRightTriggerDigital(report[6] >> 3);
+            m_ReportArgs.SetLeftShoulderDigital(report[7] >> 0);
+            m_ReportArgs.SetRightShoulderDigital(report[7] >> 1);
+            m_ReportArgs.SetLeftTriggerDigital(report[7] >> 2);
+            m_ReportArgs.SetRightTriggerDigital(report[7] >> 3);
 
-            m_ReportArgs.SetLeftThumb(report[6] >> 6);
-            m_ReportArgs.SetRightThumb(report[6] >> 7);
+            m_ReportArgs.SetLeftThumb(report[7] >> 6);
+            m_ReportArgs.SetRightThumb(report[7] >> 7);
             
             // TODO: dafuq?!
             // http://forums.pcsx2.net/attachment.php?aid=57420
