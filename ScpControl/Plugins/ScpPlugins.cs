9bb5ea2163db6c72b1b23fc7f665490c3c3bb3b4
Benjamin Höglinger <nefarius@dhmx.at>
2015-11-11 19:50:44 +0100

    Added ScpPlugins class skeleton

---
+++ ScpControl/Plugins/ScpPlugins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScpControl.ScpCore;
+
+namespace ScpControl.Plugins
+{
+    public class ScpPlugins : SingletonBase<ScpPlugins>
+    {
+
+    }
+}
