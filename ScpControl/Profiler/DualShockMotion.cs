e4cdf89ab79db34e2b730c2a3b0f0fb8b418779c
Benjamin Höglinger <nefarius@dhmx.at>
2015-11-15 20:22:18 +0100

    Added class for accel/gyro

---
+++ ScpControl/Profiler/DualShockMotion.cs
@@ -0,0 +1,16 @@
+﻿namespace ScpControl.Profiler
+{
+    public class DsAccelerometer
+    {
+        public short X { get; set; }
+        public short Y { get; set; }
+        public short Z { get; set; }
+    }
+
+    public class DsGyroscope
+    {
+        public short Roll { get; set; }
+        public short Yaw { get; set; }
+        public short Pitch { get; set; }
+    }
+}
