8e83e4890522557d072787d6b918293d0239007a
Benjamin Höglinger <nefarius@dhmx.at>
2015-11-23 23:41:02 +0100

    Implementing View Model

---
+++ ScpProfiler/DualShockProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PropertyChanged;
+using ScpControl.Profiler;
+
+namespace ScpProfiler
+{
+    [ImplementPropertyChanged]
+    public class DualShockProfileViewModel
+    {
+        public DualShockProfileViewModel()
+        {
+            CurrentProfile = new DualShockProfile();
+        }
+
+        public DualShockProfile CurrentProfile { get; set; }
+    }
+}
