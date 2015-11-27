17b89cf0a3248bf25349e29d028557975b3e2f67
Benjamin Höglinger <nefarius@dhmx.at>
2015-11-27 23:55:35 +0100

    Removed unused code

--- ScpProfiler/MainWindow.xaml.cs
+++ ScpProfiler/MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using ScpControl;
 using ScpControl.Profiler;
 using ScpControl.ScpCore;
