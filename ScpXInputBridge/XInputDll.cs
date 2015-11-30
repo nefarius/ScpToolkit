2997edfe885b0b6937dfaf0266323f7d0063a032
Benjamin Höglinger <nefarius@dhmx.at>
2015-11-30 21:33:14 +0100

    Split class

--- ScpXInputBridge/XInputDll.cs
+++ ScpXInputBridge/XInputDll.cs
@@ -211,63 +211,6 @@ namespace ScpXInputBridge
 
         #endregion
 
-        #region Native structs
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_GAMEPAD
-        {
-            public ushort wButtons;
-            public byte bLeftTrigger;
-            public byte bRightTrigger;
-            public short sThumbLX;
-            public short sThumbLY;
-            public short sThumbRX;
-            public short sThumbRY;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_STATE
-        {
-            public uint dwPacketNumber;
-            public XINPUT_GAMEPAD Gamepad;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_VIBRATION
-        {
-            public uint wLeftMotorSpeed;
-            public uint wRightMotorSpeed;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_CAPABILITIES
-        {
-            public byte Type;
-            public byte SubType;
-            public ushort Flags;
-            public XINPUT_GAMEPAD Gamepad;
-            public XINPUT_VIBRATION Vibration;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_BATTERY_INFORMATION
-        {
-            public byte BatteryType;
-            public byte BatteryLevel;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct XINPUT_KEYSTROKE
-        {
-            public ushort VirtualKey;
-            public char Unicode;
-            public ushort Flags;
-            public byte UserIndex;
-            public byte HidCode;
-        }
-
-        #endregion
-
         #region Delegates for GetProcAddress
 
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
