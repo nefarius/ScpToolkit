ada7db97ba2f66355d38e08f1dd8eb0a997960a2
Benjamin Höglinger <nefarius@dhmx.at>
2015-12-06 10:24:54 +0100

    Fixed crash on loading damaged profile files Updated version

--- ScpControl/Profiler/DualShockProfileManager.cs
+++ ScpControl/Profiler/DualShockProfileManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -22,23 +21,40 @@ namespace ScpControl.Profiler
 
         private DualShockProfileManager()
         {
-            Profiles = new List<DualShockProfile>();
-
             LoadProfiles();
 
             _fswProfileFiles.Changed += FswProfileFilesOnChanged;
             _fswProfileFiles.Created += FswProfileFilesOnChanged;
             _fswProfileFiles.Renamed += FswProfileFilesOnChanged;
             _fswProfileFiles.Deleted += FswProfileFilesOnChanged;
-            
-            _fswProfileFiles.Error += (sender, args) =>
-            {
-                Log.ErrorFormat("Unexpected error in Profiles FileSystemWatcher: {0}", args.GetException());
-            };
+
+            _fswProfileFiles.Error +=
+                (sender, args) =>
+                {
+                    Log.ErrorFormat("Unexpected error in Profiles FileSystemWatcher: {0}", args.GetException());
+                };
 
             _fswProfileFiles.EnableRaisingEvents = true;
         }
 
+        public static string ProfileFileFilter
+        {
+            get { return "*.xml"; }
+        }
+
+        public static string ProfilesPath
+        {
+            get { return Path.Combine(GlobalConfiguration.AppDirectory, "Profiles"); }
+        }
+
+        /// <summary>
+        ///     Gets a list of all available profiles loaded from memory.
+        /// </summary>
+        public IReadOnlyList<DualShockProfile> Profiles { get; private set; }
+
+        /// <summary>
+        ///     Reads all XML files from Profiles directory.
+        /// </summary>
         private void LoadProfiles()
         {
             lock (this)
@@ -57,14 +73,12 @@ namespace ScpControl.Profiler
 
                         Log.InfoFormat("Successfully loaded profile {0}", profile.Name);
                     }
-                    catch (Exception ex)
+                    catch (SerializationException ex)
                     {
                         Log.ErrorFormat("Couldn't load profile from file {0}, maybe it's damaged or outdated", file);
                         Log.DebugFormat("Profile load error: {0}", ex);
                     }
                 }
-
-                if (!profiles.Any()) return;
 
                 Profiles = profiles.AsReadOnly();
             }
@@ -84,21 +98,6 @@ namespace ScpControl.Profiler
             LoadProfiles();
         }
 
-        public static string ProfileFileFilter
-        {
-            get { return "*.xml"; }
-        }
-
-        public static string ProfilesPath
-        {
-            get { return Path.Combine(GlobalConfiguration.AppDirectory, "Profiles"); }
-        }
-
-        /// <summary>
-        ///     Gets a list of all available profiles loaded from memory.
-        /// </summary>
-        public IReadOnlyList<DualShockProfile> Profiles { get; private set; }
-
         /// <summary>
         ///     Feeds the supplied HID report through all loaded mapping profiles.
         /// </summary>
@@ -112,46 +111,46 @@ namespace ScpControl.Profiler
         }
 
         /// <summary>
-        ///     Stores a new <see cref="DualShockProfile"/> or overwrites an existing one.
+        ///     Stores a new <see cref="DualShockProfile" /> or overwrites an existing one.
         /// </summary>
-        /// <param name="profile">The <see cref="DualShockProfile"/> to save.</param>
+        /// <param name="profile">The <see cref="DualShockProfile" /> to save.</param>
         public void SubmitProfile(DualShockProfile profile)
         {
             Save(profile, Path.Combine(GlobalConfiguration.ProfilesPath, profile.FileName));
         }
 
         /// <summary>
-        ///     Removes a given <see cref="DualShockProfile"/>.
+        ///     Removes a given <see cref="DualShockProfile" />.
         /// </summary>
-        /// <param name="profile">The <see cref="DualShockProfile"/>to remove.</param>
+        /// <param name="profile">The <see cref="DualShockProfile" />to remove.</param>
         public void RemoveProfile(DualShockProfile profile)
         {
             File.Delete(Path.Combine(GlobalConfiguration.ProfilesPath, profile.FileName));
         }
 
+        /// <summary>
+        ///     Loads a <see cref="DualShockProfile"/> from a file.
+        /// </summary>
+        /// <param name="file">The file to read.</param>
+        /// <returns>The deserialized <see cref="DualShockProfile"/>.</returns>
         private static DualShockProfile Load(string file)
         {
-            try
+            var serializer = new DataContractSerializer(typeof (DualShockProfile));
+
+            using (var fs = File.OpenText(file))
             {
-                Log.DebugFormat("Loading profile from file: {0}", file);
-
-                var serializer = new DataContractSerializer(typeof (DualShockProfile));
-
-                using (var fs = File.OpenText(file))
+                using (var xml = XmlReader.Create(fs))
                 {
-                    using (var xml = XmlReader.Create(fs))
-                    {
-                        return (DualShockProfile) serializer.ReadObject(xml);
-                    }
+                    return (DualShockProfile) serializer.ReadObject(xml);
                 }
-            }
-            catch (Exception ex)
-            {
-                Log.FatalFormat("Couldn't load profile {0} due to an unexpected error: {1}", file, ex);
-                return null;
             }
         }
 
+        /// <summary>
+        ///     Saves a <see cref="DualShockProfile"/> as a file.
+        /// </summary>
+        /// <param name="profile">The <see cref="DualShockProfile"/> to save.</param>
+        /// <param name="file">The file name to save the <see cref="DualShockProfile"/> to.</param>
         private static void Save(DualShockProfile profile, string file)
         {
             var serializer = new DataContractSerializer(profile.GetType());
@@ -161,7 +160,7 @@ namespace ScpControl.Profiler
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
 
-            using (var xml = XmlWriter.Create(file, new XmlWriterSettings { Indent = true }))
+            using (var xml = XmlWriter.Create(file, new XmlWriterSettings {Indent = true}))
             {
                 serializer.WriteObject(xml, profile);
             }
