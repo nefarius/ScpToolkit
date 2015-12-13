1f4d972bf4ce2738d8ca761904eef3ff407085b1
Benjamin Höglinger <nefarius@dhmx.at>
2015-12-13 22:30:56 +0100

    Updated version Split setup into sub-features

--- ScpPair/Properties/CommonInfo.cs
+++ ScpPair/Properties/CommonInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.6.200.15347")]
-[assembly: AssemblyFileVersion("1.6.200.15347")]
+[assembly: AssemblyVersion("1.6.201.15347")]
+[assembly: AssemblyFileVersion("1.6.201.15347")]
