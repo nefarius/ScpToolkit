723553578eb883ff09f59a12d9fe638277ebb51c
Benjamin Höglinger <nefarius@dhmx.at>
2016-01-10 14:00:01 +0100

    Updated version

--- ScpDebugInfoCollector/Properties/CommonInfo.cs
+++ ScpDebugInfoCollector/Properties/CommonInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.6.237.16009")]
-[assembly: AssemblyFileVersion("1.6.237.16009")]
+[assembly: AssemblyVersion("1.6.238.16010")]
+[assembly: AssemblyFileVersion("1.6.238.16010")]
