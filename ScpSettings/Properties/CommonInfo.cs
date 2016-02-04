639379108cf86515574cc5dad6569df8234f6bd1
Benjamin Höglinger <nefarius@dhmx.at>
2016-02-04 19:47:09 +0100

    Updated version

--- ScpSettings/Properties/CommonInfo.cs
+++ ScpSettings/Properties/CommonInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.6.249.16030")]
-[assembly: AssemblyFileVersion("1.6.249.16030")]
+[assembly: AssemblyVersion("1.6.250.16034")]
+[assembly: AssemblyFileVersion("1.6.250.16034")]
